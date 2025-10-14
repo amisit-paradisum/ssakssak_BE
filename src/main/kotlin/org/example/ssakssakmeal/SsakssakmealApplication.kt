4eef82a15b57efaab591f80213b30e7bb4b5d478
whitefish <whitefish822@gmail.com>
2025-10-14 19:45:35 +0900

    init
    spring boot

---
+++ src/main/kotlin/org/example/ssakssakmeal/SsakssakmealApplication.kt
@@ -0,0 +1,11 @@
+package org.example.ssakssakmeal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SsakssakmealApplication
+
+fun main(args: Array<String>) {
+    runApplication<SsakssakmealApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/ssakssakmeal/SsakssakmealApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.ssakssakmeal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SsakssakmealApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
